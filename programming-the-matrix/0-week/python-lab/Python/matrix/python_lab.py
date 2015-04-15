7831c97247cda82f7c705d45c3a26841f8d05c8c
josiah14 <praenato14@gmail.com>
2015-04-14 21:42:33 -0500

    finish python-lab

--- programming-the-matrix/0-week/python-lab/Python/matrix/python_lab.py
+++ programming-the-matrix/0-week/python-lab/Python/matrix/python_lab.py
@@ -150,27 +150,27 @@ representation_dict = None #{ ... }
 id2salary = {0:1000.0, 1:1200.50, 2:990}
 names = ['Larry', 'Curly', 'Moe']
 # Replace { ... } with a one-line dictionary comprehension that uses id2salary and names.
-listdict2dict = None #{ ... }
+listdict2dict = { names[index]: id2salary[index] for index in id2salary.keys() }
 
 ## 26: (Task 26) Procedure nextInts
 # Complete the procedure definition by replacing [ ... ] with a one-line list comprehension
-def nextInts(L): return None #[ ... ]
+def nextInts(L): return [ x + 1 for x in L ]
 
 ## 27: (Task 27) Procedure cubes
 # Complete the procedure definition by replacing [ ... ] with a one-line list comprehension
-def cubes(L): return None #[ ... ]
+def cubes(L): return [ x**3 for x in L ]
 
 ## 28: (Task 28) Procedure dict2list
 # Input: a dictionary dct and a list keylist consisting of the keys of dct
 # Output: the list L such that L[i] is the value associated in dct with keylist[i]
 # Example: dict2list({'a':'A', 'b':'B', 'c':'C'},['b','c','a']) should equal ['B','C','A']
 # Complete the procedure definition by replacing [ ... ] with a one-line list comprehension
-def dict2list(dct, keylist): return None #[ ... ]
+def dict2list(dct, keylist): return [ dct[key] for key in keylist ]
 
 ## 29: (Task 29) Procedure list2dict
 # Input: a list L and a list keylist of the same length
 # Output: the dictionary that maps keylist[i] to L[i] for i=0,1,...len(L)-1
 # Example: list2dict(['A','B','C'],['a','b','c']) should equal {'a':'A', 'b':'B', 'c':'C'}
 # Complete the procedure definition by replacing { ... } with a one-line dictionary comprehension
-def list2dict(L, keylist): return None #{ ... }
+def list2dict(L, keylist): return { kv[0]:kv[1] for kv in zip(keylist, L) }
 
