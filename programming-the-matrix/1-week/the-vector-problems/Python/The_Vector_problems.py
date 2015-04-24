f774b227709a18e1ab3266061bdeef0a88af61c7
josiah14 <praenato14@gmail.com>
2015-04-23 23:54:44 -0500

    save work on the vector problems

--- programming-the-matrix/1-week/the-vector-problems/Python/The_Vector_problems.py
+++ programming-the-matrix/1-week/the-vector-problems/Python/The_Vector_problems.py
@@ -1,3 +1,6 @@
+import vector
+import GF2
+
 # version code ef5291f09f60+
 coursera = 1
 # Please fill out this stencil and submit using the provided submission script.
@@ -6,34 +9,27 @@ coursera = 1
 
 from GF2 import one
 
-
-
 ## 1: (Problem 1) Vector Addition Practice 1
 #Please express each answer as a list of numbers
 p1_v = [-1, 3]
 p1_u = [0, 4]
-p1_v_plus_u = [...]
-p1_v_minus_u = [...]
-p1_three_v_minus_two_u = [...]
-
-
+p1_v_plus_u = vector.add(p1_v, p1_u)
+p1_v_minus_u = vector.minus(p1_v, p1_u)
+p1_three_v_minus_two_u = vector.minus(vector.scalar_mult(3, p1_v), vector.scalar_mult(2, p1_u))
 
 ## 2: (Problem 2) Vector Addition Practice 2
 p2_u = [-1,  1, 1]
 p2_v = [ 2, -1, 5]
-p2_v_plus_u = [...]
-p2_v_minus_u = [...]
-p2_two_v_minus_u = [...]
-p2_v_plus_two_u = [...]
-
-
+p2_v_plus_u = vector.add(p2_v, p2_u)
+p2_v_minus_u = vector.minus(p2_v, p2_u)
+p2_two_v_minus_u = vector.minus(vector.scalar_mult(2, p2_v), p2_u)
+p2_v_plus_two_u = vector.add(p2_v, vector.scalar_mult(2, p2_u))
 
 ## 3: (Problem 3) Vector Addition Practice 3
-# Write your answer using GF2's one instead of the number 1
-p3_vector_sum_1 = [...]
-p3_vector_sum_2 = [...]
-
-
+p3_v = [GF2.zero, GF2.one, GF2.one]
+p3_u = [GF2.one, GF2.one, GF2.one]
+p3_vector_sum_1 = vector.add(p3_v, p3_u)
+p3_vector_sum_2 = vector.add(p3_v, p3_u, p3_u)
 
 ## 4: (Problem 4) GF2 Vector Addition A
 # Please express your solution as a subset of the letters {'a','b','c','d','e','f'}.
@@ -41,37 +37,39 @@ p3_vector_sum_2 = [...]
 #   a (1100000), b (0110000), and c (0011000).
 # The answer should be an empty set, written set(), if the given vector u cannot
 # be written as the sum of any subset of the vectors a, b, c, d, e, and f.
+p4_vectors = {
+    'a': [GF2.one, GF2.one, GF2.zero, GF2.zero, GF2.zero, GF2.zero, GF2.zero],
+    'b': [GF2.zero, GF2.one, GF2.one, GF2.zero, GF2.zero, GF2.zero, GF2.zero],
+    'c': [GF2.zero, GF2.zero, GF2.one, GF2.one, GF2.zero, GF2.zero, GF2.zero],
+    'd': [GF2.zero, GF2.zero, GF2.zero, GF2.one, GF2.one, GF2.zero, GF2.zero],
+    'e': [GF2.zero, GF2.zero, GF2.zero, GF2.zero, GF2.one, GF2.one, GF2.zero],
+    'f': [GF2.zero, GF2.zero, GF2.zero, GF2.zero, GF2.zero, GF2.one, GF2.one]
+}
 
-u_0010010 = ...
-u_0100010 = ...
-
-
+u0 = [GF2.zero, GF2.zero, GF2.one, GF2.zero, GF2.zero, GF2.one, GF2.zero]
+u_0010010 = set(vector.flatten2([ x for x in vector.permutations_unordered_no_repeat(list(p4_vectors)) if vector.add(p4_vectors[x[0]], p4_vectors[x[1]]) == u0 ]))
+u1 = [GF2.zero, GF2.zero, GF2.one, GF2.zero, GF2.zero, GF2.one, GF2.zero]
+u_0100010 = None
 
 ## 5: (Problem 5) GF2 Vector Addition B
 # Use the same format as the previous problem
 
-v_0010010 = ...
-v_0100010 = ...
-
-
+v_0010010 = None
+v_0100010 = None
 
 ## 6: (Problem 6) Solving Linear Equations over GF(2)
 #You should be able to solve this without using a computer.
-x_gf2 = [...]
-
-
+x_gf2 = None
 
 ## 7: (Problem 7) Formulating Equations using Dot-Product
 #Please provide each answer as a list of numbers
-v1 = [...]
-v2 = [...]
-v3 = [...]
-
-
+v1 = None
+v2 = None
+v3 = None
 
 ## 8: (Problem 8) Practice with Dot-Product
-uv_a = ...
-uv_b = ...
-uv_c = ...
-uv_d = ...
+uv_a = None
+uv_b = None
+uv_c = None
+uv_d = None
 
---
+++ programming-the-matrix/1-week/the-vector-problems/Python/vector.py
@@ -0,0 +1,40 @@
+from functools import reduce
+from itertools import count
+
+def add(*vectors):
+    return [ reduce(lambda x0, x1: x0 + x1, xs) for xs in zip(*vectors) ]
+
+def minus(*vectors):
+    return [ reduce(lambda x0, x1: x0 - x1, xs) for xs in zip(*vectors) ]
+
+def scalar_mult(constant, vector):
+    return list(map(lambda x: constant * x, vector))
+
+def inverse_factorial(num):
+    for i in count(1):
+        num = num / i
+        if num == 1: return i
+        if num < 1: raise ValueError("number was not an even factorial.")
+
+def inverse_natural_sum(num, start):
+    for i in range(start, 0, -1):
+        num = num - i
+        if num == 0: return i
+        if num < 0: raise ValueError("number is not a natural sum.")
+    
+def permutations_unordered_no_repeat(collection):
+    def reducer(acc, x):
+        if collection == None: raise ValueError("cannot provide NoneType")
+        if acc == None or acc == collection[0]:
+            [ (acc, y) for y in collection[1:] ] + [ (x, y) for y in collection[2:] ]
+        else:
+            acc + [ (x, y)
+                    for y in collection[len(collection) - 1 -
+                                        inverse_natural_sum(
+                                            len(acc),
+                                            len(collection) - 1) + 2:] ]
+    return reduce(reducer, collection)
+
+def flatten2(collection):
+    return [ y for x in collection for y in x ]
+        
