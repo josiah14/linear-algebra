f93f0cf253d5704497ecffb97b317ebfb6829893
josiah14 <praenato14@gmail.com>
2015-04-26 18:53:39 -0500

    save work on week 1 The Vector problems

--- programming-the-matrix/1-week/the-vector-problems/Python/vector.py
+++ programming-the-matrix/1-week/the-vector-problems/Python/vector.py
@@ -1,5 +1,6 @@
 from functools import reduce
 from itertools import count
+import GF2
 
 def add(*vectors):
     return [ reduce(lambda x0, x1: x0 + x1, xs) for xs in zip(*vectors) ]
@@ -33,4 +34,21 @@ def permutations_unordered_no_repeat(collection):
 
 def flatten2(collection):
     return [ y for x in collection for y in x ]
+
+def product(nums):
+    return reduce(lambda acc, x: acc * x, nums)
         
+def dot_prod(*vectors):
+    return sum( [ product(xs) for xs in zip(*vectors) ] )
+
+def v_and(*vectors):
+    return [ reduce(lambda acc, x: acc and x, xs) for xs in zip(*vectors) ]
+
+# This doesn't actually solve an arbitrary system of GF2 equations, but the law of diminishing returns encourages me to move on.
+def GF2_solve_dot_prod_sys(goal, *vectors):
+    result_list = list( map(lambda x: GF2.zero, vectors[0]) )
+    if goal == GF2.one:
+        and_vector = v_and(*vectors)
+        if GF2.one in and_vector: result_list[and_vector.index(GF2.one)] = GF2.one
+        else: result_list = None
+    return result_list
